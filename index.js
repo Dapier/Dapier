71ecd8d293a1f371e38b0c2a1876366394b14bb1
Dapier <josuea501@gmail.com>
2022-04-30 09:35:02 -0500

    Adding portfolio link

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const fs = require('fs').promises
 
 const iconsSizePlaceholder = /%{{icon_size}}%/g
+const timePlaceholder = /%{{time}}%/g
+
+
 const iconSize = '21px'
 ;(async () =>{
     const markdownTemplate = await fs.readFile('./README.md.tpl', {encoding: 'utf-8'})
     //Changing icon size to all icons
     const newMarkdown = markdownTemplate
     .replace(iconsSizePlaceholder, iconSize)
-   
+    .replace(time, newTime)
     
     //Adding changes into readme
     await fs.writeFile('./README.md', newMarkdown)
