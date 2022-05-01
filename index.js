9c6918ad3c7a84458d872fb3a72e5afaf4c2f6f0
Dapier <josuea501@gmail.com>
2022-05-01 08:43:45 -0500

    fixing greeting time

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const fs = require('fs').promises
 const iconsSizePlaceholder = /%{{icon_size}}%/g
 const timePlaceholder = /%{{greeting}}%/g
 
-var d = new Date();
-var time = d.getHours();
-var currentTime
-console.log(currentTime);
-if (time < 12) {
-  currentTime = '☀ Good Morning ☀'
-}
-if (time > 12) {
-  currentTime = "Good afternoon!";
-}
-if (time == 12) {
-    currentTime = "🌕 Good Night! 🌕";
-}
+const date = new Date();
+let hours = date.getHours();
+let greetingTime = (hours < 12) ? '☀ Good Morning ☀' : ((hours <= 18 && hours >=12)? 'Good afternoon!' : "🌕 Good Night! 🌕" )
+
+
+// console.log(currentTime);
+// if (time < 12) {
+//   currentTime = "Good afternoon!";
+// }
+// if (time > 12) {
+//   currentTime = '☀ Good Morning ☀'
+
+// }
+// if (time == 12) {
+//     currentTime = "🌕 Good Night! 🌕";
+// }
+
 
 const iconSize = '21px'
 ;(async () =>{
@@ -23,7 +27,7 @@ const iconSize = '21px'
     //Changing icon size to all icons
     const newMarkdown = markdownTemplate
     .replace(iconsSizePlaceholder, iconSize)
-    .replace(timePlaceholder, currentTime)
+    .replace(timePlaceholder, greetingTime)
     
     //Adding changes into readme
     await fs.writeFile('./README.md', newMarkdown)
